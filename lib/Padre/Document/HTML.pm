c4a0986d99df1552b1ab45e115e9db253b30ff1e
Fayland 林 <fayland@gmail.com>
2008-12-19 05:35:14 +0000

    Import of FAYLAND/Padre-Plugin-HTML-0.05 from CPAN.
    gitpan-cpan-distribution: Padre-Plugin-HTML
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         FAYLAND/Padre-Plugin-HTML-0.05.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

---
+++ lib/Padre/Document/HTML.pm
@@ -0,0 +1,20 @@
+package Padre::Document::HTML;
+
+use 5.008;
+use strict;
+use warnings;
+use Carp            ();
+use Padre::Document ();
+use Wx ':everything';
+
+our $VERSION = '0.22';
+our @ISA     = 'Padre::Document';
+
+sub get_command {
+	my $self     = shift;
+
+	my $filename = $self->filename;	
+	Wx::LaunchDefaultBrowser($filename);
+}
+
+1;
