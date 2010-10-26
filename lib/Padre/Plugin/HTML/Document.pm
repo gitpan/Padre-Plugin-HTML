baf3af84d033b82defb0d4a1e8fc745c3f90e0ef
أحمد محمد زواوي - Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2010-10-26 16:41:29 +0000

    Import of AZAWAWI/Padre-Plugin-HTML-0.11 from CPAN.
    gitpan-cpan-distribution: Padre-Plugin-HTML
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         AZAWAWI/Padre-Plugin-HTML-0.11.tar.gz
    gitpan-cpan-author:       AZAWAWI
    gitpan-cpan-maturity:     released

--- lib/Padre/Plugin/HTML/Document.pm
+++ lib/Padre/Plugin/HTML/Document.pm
@@ -1,22 +1,59 @@
 package Padre::Plugin::HTML::Document;
+BEGIN {
+  $Padre::Plugin::HTML::Document::VERSION = '0.11';
+}
 
 use 5.008;
 use strict;
 use warnings;
 use Carp            ();
 use Padre::Document ();
-use Padre::Wx ();
+use Padre::Wx       ();
 
-our $VERSION = '0.10';
-our @ISA     = 'Padre::Document';
+our @ISA = 'Padre::Document';
 
 sub get_command {
-	my $self     = shift;
+	my $self = shift;
 
-	my $filename = $self->filename;	
+	my $filename = $self->filename;
 	Wx::LaunchDefaultBrowser($filename);
 }
 
 sub comment_lines_str { return [ '<!--', '-->' ] }
 
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Padre::Plugin::HTML::Document
+
+=head1 VERSION
+
+version 0.11
+
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+Fayland Lam <fayland@gmail.com>
+
+=item *
+
+Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2010 by Fayland Lam.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
